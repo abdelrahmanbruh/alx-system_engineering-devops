af3c46130244422efc9739b7fefd910a118efbd0
abdelrahmanbruh <abdalrahman.moushref3@gmail.com>
2023-07-29 00:24:28 +0200

    cuh

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+
+/**
+ * infinite_while - idg
+ * Return: 0
+*/
+int infinite_while(void)
+{
+while (1)
+{
+sleep(1);
+}
+return (0);
+}
+
+/**
+ * main - IDK
+ * Return: 0 
+*/
+int main(void)
+{
+int children_processes = 0;
+pid_t pid;
+while (children_processes < 5)
+{
+pid = fork();
+if (!pid)
+break;
+printf("Zombie process created, PID: %i\n", (int)pid);
+children_processes++;
+}
+if (pid != 0)
+infinite_while();
+return (0);
+}
